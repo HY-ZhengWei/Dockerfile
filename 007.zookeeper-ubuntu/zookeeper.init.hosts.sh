2bd026c2ba07cfe407f29abb1e5c520d51059769
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-06-01 16:20:25 +0800

    动态Zookeeper版本环境

--- 007.zookeeper-ubuntu/zookeeper.init.hosts.sh
+++ 007.zookeeper-ubuntu/zookeeper.init.hosts.sh
@@ -2,6 +2,6 @@
 
 
 # 设置host name
-echo "172.17.0.6  zookeeper01" >> /etc/hosts
-echo "172.17.0.7  zookeeper02" >> /etc/hosts
-echo "172.17.0.8  zookeeper03" >> /etc/hosts
+echo "172.17.0.8  zookeeper01" >> /etc/hosts
+echo "172.17.0.9  zookeeper02" >> /etc/hosts
+echo "172.17.0.10 zookeeper03" >> /etc/hosts
