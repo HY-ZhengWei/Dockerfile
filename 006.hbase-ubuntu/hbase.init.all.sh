fe32a7b3eb32262db17718d999694f6182788565
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-06-01 16:19:31 +0800

    动态HBase版本环境

--- 006.hbase-ubuntu/hbase.init.all.sh
+++ 006.hbase-ubuntu/hbase.init.all.sh
@@ -2,15 +2,37 @@
 
 
 # 用预先设置好的HBase配置信息覆盖HBase包中的配置文件
+if [ "$1" = "hbase" ]; then
+
+# 使用HBase自带的zookeeper
 cp $HBASE_CONIFG_HY/* $HBASE_CONF_DIR
+
+elif [ "$1" = "zookeeper" ]; then
+
+# 使用独立部署的zookeeper
+cp $HBASE_CONIFG_HY_zookeeper/* $HBASE_CONF_DIR
+
+else  
+  echo "Please select initialization zookeeper mode <hbase | zookeeper>."
+  exit
+fi
+
 
 
 # 设置host name
-hbase.init.hosts.sh
+hbase.init.hosts.sh $1
 
 
 # 无密码登录
 ssh-keygen -t rsa -P "" -f /root/.ssh/id_rsa
 
-ssh-copy-id -i hbase01 
-ssh-copy-id -i hbase02
+sshpass -p root ssh-copy-id -i hbase01 
+sshpass -p root ssh-copy-id -i hbase02
+
+if [ "$1" = "zookeeper" ]; then
+
+sshpass -p root ssh-copy-id -i zookeeper01
+sshpass -p root ssh-copy-id -i zookeeper02
+sshpass -p root ssh-copy-id -i zookeeper03
+
+fi
--- 006.hbase-ubuntu/hbase.init.hosts.sh
+++ 006.hbase-ubuntu/hbase.init.hosts.sh
@@ -4,9 +4,24 @@
 # 设置host name
 echo "172.17.0.2  hadoop01"    >> /etc/hosts
 
-echo "172.17.0.6  zookeeper01" >> /etc/hosts
-echo "172.17.0.7  zookeeper02" >> /etc/hosts
-echo "172.17.0.8  zookeeper03" >> /etc/hosts
+echo "172.17.0.6  hbase01"     >> /etc/hosts
+echo "172.17.0.7  hbase02"     >> /etc/hosts
 
-echo "172.17.0.9  hbase01"     >> /etc/hosts
-echo "172.17.0.10 hbase02"     >> /etc/hosts
+
+if [ "$1" = "hbase" ]; then
+
+
+elif [ "$1" = "zookeeper" ]; then
+
+echo "172.17.0.8  zookeeper01" >> /etc/hosts
+echo "172.17.0.9  zookeeper02" >> /etc/hosts
+echo "172.17.0.10 zookeeper03" >> /etc/hosts
+
+else  
+  echo "Please select initialization zookeeper mode <hbase | zookeeper>."
+  exit
+fi
+
+
+
+
